d3185d73f54de8e7b06a0eeec4c85584c53ba5d8
pirnataly <piroznatal@gmail.com>
2024-04-13 10:30:19 +0300

    feat: solve getPowerFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -92,8 +92,10 @@ function getArgumentsCount(funcs) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return function toExponent(number) {
+    return number ** exponent;
+  };
 }
 
 /**
